190dc6629e18c03a357b8c3c05844b2394ac3593
Širhoe Biazhkovič <sblazhko@sot-te.ch>
2021-11-15 14:53:53 +0300

    Minor fix: truncate previous memcheck log

--- memcheck.sh
+++ memcheck.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+truncate -s 0 memcheck.log
 valgrind --tool=memcheck --leak-check=full --show-reachable=yes --track-origins=yes \
 	build/lottieconverter $@ 2> >(tee -a memcheck.log >&2)
