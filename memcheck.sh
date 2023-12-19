b7f2bfff4553cd13e73498ae2cf8b34f63fb4dc6
sewn <sewn@disroot.org>
2023-12-19 15:13:32 +0300

    memcheck: don't log, use sh
    Assuming this script is ran locally, without a CI running it, and
    is used for checking memory leaks, this shouldn't realistically need
    an external log file, as the person using the script would have to run,
    and then 'cat' the script, instead of having it directly in the
    terminal for convenience.

--- memcheck.sh
+++ memcheck.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
-truncate -s 0 memcheck.log
+#!/bin/sh
+
 valgrind --tool=memcheck --leak-check=full --show-reachable=yes --track-origins=yes \
-	build/lottieconverter $@ 2> >(tee -a memcheck.log >&2)
+	build/lottieconverter $@
